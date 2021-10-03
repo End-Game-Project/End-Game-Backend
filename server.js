27d25cff15068f7dfb5833f9ba07f8e720cf3210
Qamar <qalkhatib0@gmail.com>
2021-10-03 13:09:54 +0300

    API Added

--- server.js
+++ server.js
@@ -6,24 +6,30 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 const server = express();
 const PORT = process.env.PORT;
+
 server.use(cors());
-
 // Middleware to decode any request body to json(with Post req)
-server.use(express.json());
+// server.use(express.json());
 
-mongoose.connect(`${process.env.MONGO_SERVER}`);
+// mongoose.connect(`${process.env.MONGO_SERVER}`);
+const getGamesHandler = require(`./modules/game.js`)
 
 server.get('/', homeHandler);
+server.get('/getGame', getGamesHandler);
 server.get('*', notFound);
+
 
 
 function homeHandler(req, res) {
     res.status(200).send(`It's Working!`);
 }
 
+
 function notFound(req, res) {
     res.status(404).send(`404`);
 }
+
+
 
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
