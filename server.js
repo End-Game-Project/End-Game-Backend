0aea5da917d10d113b8f8651925203048a5f9e69
Qamar <qalkhatib0@gmail.com>
2021-10-03 12:21:18 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const server = express();
+const PORT = process.env.PORT;
+server.use(cors());
+
+// Middleware to decode any request body to json(with Post req)
+server.use(express.json());
+
+mongoose.connect(`${process.env.MONGO_SERVER}`);
+
+server.get('/', homeHandler);
+server.get('*', notFound);
+
+
+function homeHandler(req, res) {
+    res.status(200).send(`It's Working!`);
+}
+
+function notFound(req, res) {
+    res.status(404).send(`404`);
+}
+
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+})
